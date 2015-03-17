a77561b0fcbd34385f2485b6dba83aed733fb3f5
mouzi2012 <moulianhuan@gmail.com>
2015-03-17 14:11:13 +0800

    fix datareader

--- DataReader.h
+++ DataReader.h
@@ -1,4 +1,18 @@
 #ifndef DATAREADER_H
 #define DATAREADER_H
 // this is the file to parse data from .fbx file do not lose it again!!!!
+#include <fbxsdk.h>
+#define NAMEL 256
+class DataReader
+{
+public:
+	DataReader();
+	~DataReader();
+public:
+	void SetTarget(const char* name);
+private:
+	char m_target[NAMEL];
+	FbxManager* m_manager;
+	FbxScene* m_scene;
+};
 #endif
